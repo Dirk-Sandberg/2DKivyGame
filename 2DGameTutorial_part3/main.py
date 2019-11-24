c4ab86e395c37e8142bf1ed9aedca069a35329da
eriksandberg <eriksandbergum@gmail.com>
2019-11-24 09:11:42 -0600

    Starting part3

---
+++ 2DGameTutorial_part3/main.py
@@ -0,0 +1,49 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.clock import Clock
+
+from pipe import Pipe
+
+class Background(Widget):
+    cloud_texture = ObjectProperty(None)
+    floor_texture = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # Create textures
+        self.cloud_texture = Image(source="cloud.png").texture
+        self.cloud_texture.wrap = 'repeat'
+        self.cloud_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
+
+        self.floor_texture = Image(source="floor.png").texture
+        self.floor_texture.wrap = 'repeat'
+        self.floor_texture.uvsize = (Window.width / self.floor_texture.width, -1)
+
+    def on_size(self, *args):
+        self.cloud_texture.uvsize = (self.width / self.cloud_texture.width, -1)
+        self.floor_texture.uvsize = (self.width / self.floor_texture.width, -1)
+
+    def scroll_textures(self, time_passed):
+        # Update the uvpos of the texture
+        self.cloud_texture.uvpos = ( (self.cloud_texture.uvpos[0] + time_passed/2.0)%Window.width , self.cloud_texture.uvpos[1])
+        self.floor_texture.uvpos = ( (self.floor_texture.uvpos[0] + time_passed)%Window.width, self.floor_texture.uvpos[1])
+
+        # Redraw the texture
+        texture = self.property('cloud_texture')
+        texture.dispatch(self)
+
+        texture = self.property('floor_texture')
+        texture.dispatch(self)
+
+
+class MainApp(App):
+    def on_start(self):
+        Clock.schedule_interval(self.root.ids.background.scroll_textures, 1/60.)
+    pass
+
+
+MainApp().run()
---
+++ 2DGameTutorial_part3/pipe.py
@@ -0,0 +1,37 @@
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ObjectProperty, ListProperty
+from kivy.uix.image import Image
+
+class Pipe(Widget):
+    # Numeric attributes
+    GAP_SIZE = NumericProperty(60)
+    CAP_SIZE = NumericProperty(20) # Height of pipe_cap.png
+    pipe_center = NumericProperty(0)
+    bottom_body_position = NumericProperty(0)
+    bottom_cap_position = NumericProperty(0)
+    top_body_position = NumericProperty(0)
+    top_cap_position = NumericProperty(0)
+
+    # Texture
+    pipe_body_texture = ObjectProperty(None)
+    lower_pipe_tex_coords = ListProperty((0, 0, 1, 0, 1, 1, 0, 1))
+    top_pipe_tex_coords = ListProperty((0, 0, 1, 0, 1, 1, 0, 1))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.pipe_body_texture = Image(source="pipe_body.png").texture
+        self.pipe_body_texture.wrap = 'repeat'
+
+    def on_size(self, *args):
+        lower_body_size = self.bottom_cap_position - self.bottom_body_position
+
+        self.lower_pipe_tex_coords[5] = lower_body_size/20.
+        self.lower_pipe_tex_coords[7] = lower_body_size/20.
+
+        top_body_size = self.top - self.top_body_position
+
+        self.top_pipe_tex_coords[5] = top_body_size/20.
+        self.top_pipe_tex_coords[7] = top_body_size/20.
+
+
+
